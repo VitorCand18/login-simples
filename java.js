96aef3655192529f50f64bac77e66a076444de03
Vitor Candido <vitor.candido900@gmail.com>
2025-01-29 16:55:07 -0300

    Alteracao btnEnviar

--- java.js
+++ java.js
@@ -1,15 +1,12 @@
-function verificarLogin() {
+function verificarCampos() {
     let username = document.getElementById('username').value;  // Obtém o valor do campo de texto
+    let email = document.getElementById('useremail').value;
 
     if (username === 'vitorcand') {
         alert('Esse login não esta disponível.');
     } else {
         alert('Login válido!');
     }
-}
-function verificarEmail(){
-
-    var email = document.getElementById('useremail').value;
 
     // Verifica se o e-mail contém "@" e ".com.br"
     if (email.includes("@") && email.includes(".com.br")) {
